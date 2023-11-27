29b0aafcec686a51279aac7afef0fc870cf1120c
kosiarn <62908964+kosiarn@users.noreply.github.com>
2023-11-27 18:09:27 +0100

    add SQL DB connection in store

--- php/index.php
+++ php/index.php
@@ -9,15 +9,18 @@
     <?php
         //define SQL connection params
 
-        $sql_server_address = 'localhost';
+        $sql_server_address = '127.0.0.1';
         $sql_username = 'root';
-        $sql_password = '';
-        $sql_shop_dbname = ''; //TODO fill in the blank
+        $sql_password = 'root';
+        $sql_shop_dbname = 'classicmodels';
+        $sql_port = 3306;
+        $database = new mysqli($sql_server_address, $sql_username, $sql_password, $sql_shop_dbname, $sql_port);
 
         //search request handling
         $results = null;
         if($_POST){
-            echo("LOG: POST request sent.");
+            dd($database);
+            $results = $database->query("SELECT productName");
         }
     ?>
     <div class="container">
